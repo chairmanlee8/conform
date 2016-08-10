4c7ed871d33a17ab5a8f3988297afcc6cf1ae34e
Andrew Lee <smiley325squared@gmail.com>
2016-08-09 23:42:57 -0500

    Make it run without commenting out.

--- src/conform/core.clj
+++ src/conform/core.clj
@@ -1,5 +1,6 @@
 (ns conform.core
   (:require [instaparse.core :as insta])
+  (:require [clojure.core.match :refer [match]])
   (:require clojure.pprint)
   (:gen-class))
 
@@ -22,17 +23,19 @@
     [:statements & r] ()
     :else (throw (Exception. "Invalid parse tree, expected root element to be :statements."))))
 
-(defn dispatch-statement "" [] ())
+(defn dispatch-statement "" [] 0)
 
 (defn process-create-table
   "Process :createTable node and apply changes to model."
   [model statement]
   (match statement
-    [:createTable [:identifier id] [:columns cols]] (noop))
+    [:createTable [:identifier id] [:columns cols]] (0))
     :else (throw (Exception. "Unexpected :createTable structure.")))
 
 (defn process-column
-  "Process :column node, returns Column record")
+  "Process :column node, returns Column record"
+  []
+  0)
 
 (defn generate-type-string
   "Generate a canonical SQL type spec from a :dataType node."
